b4231fcc3c7dae1085e28d5845f03c972f10a71f
Rahul255 <rahulmohan255@gmail.com>
2021-02-24 14:21:28 +0530

    file added

--- reactmedicalstorefrontend/src/utils/Config.js
+++ reactmedicalstorefrontend/src/utils/Config.js
@@ -4,13 +4,14 @@ class Config {
         static companyApiUrl = "http://127.0.0.1:8000/api/company/";
         static companyBankApiUrl = "http://127.0.0.1:8000/api/companybank/";
         static companyOnly = "http://127.0.0.1:8000/api/companyonly/";
+        static medicineApiUrl = "http://127.0.0.1:8000/api/medicine/";
         static homeUrl = "/home";
         static logoutPageUrl = "/logout";
 
         static sidebarItem = [
                 {"index":"0","title":"Home","url":"/home","icons":"home"},
                 {"index":"1","title":"Company","url":"/company","icons":"assessment"},
-                {"index":"1","title":"Add Company","url":"/addMedicine","icons":"assessment"},
+                {"index":"2","title":"Add Company","url":"/addMedicine","icons":"assessment"},
         ];
 }
 
