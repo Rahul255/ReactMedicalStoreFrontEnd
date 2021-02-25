9e54036a7b3e6a9d3759bd9fd54bc46b12f4e095
Rahul255 <rahulmohan255@gmail.com>
2021-02-25 11:43:35 +0530

    file added

--- reactmedicalstorefrontend/src/pages/MedicineAddComponent.js
+++ reactmedicalstorefrontend/src/pages/MedicineAddComponent.js
@@ -1,4 +1,3 @@
- 
 import React from "react";
 import AuthHandler from "../utils/AuthHandler";
 import APIHandler from "../utils/APIHandler";
--- reactmedicalstorefrontend/src/pages/MedicineManageComponent.js
+++ reactmedicalstorefrontend/src/pages/MedicineManageComponent.js
@@ -155,13 +155,13 @@ class MedicineManageComponent extends React.Component {
                                 <div className="header">
                                     {this.state.dataLoaded == false?(
                                     <div className="text-center">
-                                        <div class="preloader pl-size-xl">
-                                                    <div class="spinner-layer">
-                                                        <div class="circle-clipper left">
-                                                            <div class="circle"></div>
+                                        <div className="preloader pl-size-xl">
+                                                    <div className="spinner-layer">
+                                                        <div className="circle-clipper left">
+                                                            <div className="circle"></div>
                                                         </div>
-                                                        <div class="circle-clipper right">
-                                                            <div class="circle"></div>
+                                                        <div className="circle-clipper right">
+                                                            <div className="circle"></div>
                                                         </div>
                                                     </div>
                                                 </div>
@@ -389,8 +389,8 @@ class MedicineManageComponent extends React.Component {
                           id="qty_in_strip"
                           name="qty_in_strip"
                           className="form-control"
-                          placeholder="Enter Description"
-                          defaultValue={this.state.qty_in_strip}
+                          placeholder="Enter qty in strip"
+                          value={this.state.qty_in_strip}
                         />
                       </div>
                     </div>
