1784d0022d3ca0c725089c32516720ac3e5abb60
Rahul255 <rahulmohan255@gmail.com>
2021-02-26 08:54:12 +0530

    file added

--- reactmedicalstorefrontend/src/utils/Config.js
+++ reactmedicalstorefrontend/src/utils/Config.js
@@ -13,6 +13,7 @@ class Config {
                 {"index":"1","title":"Company","url":"/company","icons":"assessment"},
                 {"index":"2","title":"Add Medicine","url":"/addMedicine","icons":"assessment"},
                 {"index":"3","title":"Manage Medicine","url":"/manageMedicine","icons":"assessment"},
+                {"index":"4","title":"Manage Company Account","url":"/manageCompanyAccount","icons":"assessment"},
         ];
 }
 
